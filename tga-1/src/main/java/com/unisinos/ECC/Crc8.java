d75894957783e1278213672ba2781721776b2f56
Mateus B. Melchiades <mateusbmelchiades@gmail.com>
2021-10-05 11:26:06 -0300

    CRC-8 functional

---
+++ tga-1/src/main/java/com/unisinos/ECC/Crc8.java
@@ -0,0 +1,34 @@
+package com.unisinos.ECC;
+
+import java.util.Arrays;
+
+public class Crc8 implements Ecc {
+    private static final boolean[] G = {true, false, false, false, false, false, true, true, true}; // 0x107
+
+    @Override
+    public byte[] generateEcc(byte[] buffer) {
+        boolean[] bitstream = new boolean[(buffer.length + 1) * 8];
+        for(int i = 0 ; i < buffer.length ; i++) {
+            for (int j = 0 ; j < 8 ; j++)
+                bitstream[i*8 + j] = (buffer[i] & (byte)(128 / Math.pow(2, j))) != 0;
+        }
+
+        for(int i = 0 ; i < buffer.length * 8 ; i++) {
+            if(bitstream[i]) {
+                for(int c = i ; c < i + 9 ; c++)
+                    bitstream[c] ^= G[c-i];
+            }
+        }
+
+        byte[] ret = {0};
+        for(int i = 0 ; i < 8 ; i++)
+            if(bitstream[bitstream.length - 8 + i]) ret[0] |= 1 << (7 - i);
+
+        return ret;
+    }
+
+    @Override
+    public boolean checksum(byte[] buffer) {
+        return false;
+    }
+}
