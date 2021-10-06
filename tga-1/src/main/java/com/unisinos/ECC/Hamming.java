2e783dc3d08137585df57f00296c9725c6eaec56
Joao Augusto Tonial Laner <joaoaugustolaner@Joaos-MacBook-Pro.local>
2021-10-06 08:24:56 -0300

    Studying Hamming Approach

---
+++ tga-1/src/main/java/com/unisinos/ECC/Hamming.java
@@ -0,0 +1,19 @@
+//package com.unisinos.ECC;
+//
+//public class Hamming implements Ecc{
+//
+//
+//    @Override
+//    public byte[] generateEcc(byte[] buffer) {
+//
+//        for (int i = 0; i < buffer.length; i++) {
+//            String str = Integer.toBinaryString(buffer[i]);
+//            System.out.println(str);
+//        }
+//    }
+//
+//    @Override
+//    public boolean checksum(byte[] buffer) {
+//        return false;
+//    }
+//}
